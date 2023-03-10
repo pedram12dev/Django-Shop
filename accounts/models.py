25a4c45757c48455f55d409ec0165059a062b756
Peram Najafi <95505162+pedram12dev@users.noreply.github.com>
2023-03-10 21:49:23 +0330

    add accounts app and file

---
+++ accounts/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
+""" create base user """
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    """ import user manager """
+    objects = UserManager()
+
+    """ login user field """
+    USERNAME_FIELD = 'phone_number'
+
+    """ request field when create superuser """
+    REQUIRED_FIELDS = ['email', 'full_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+class OtpCode(models.Model):
+    phone_number = models.CharField(max_length=11)
+    code = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.phone_number} - {self.code} - {self.created}'
+
+
